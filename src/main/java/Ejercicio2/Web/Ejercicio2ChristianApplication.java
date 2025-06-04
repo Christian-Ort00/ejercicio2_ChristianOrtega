10ec908b4e12df0e7a2e75684a7c77b64d673936
Christian-Ort00 <cortega20867@ufide.ac.cr>
2025-06-04 17:14:09 -0600

    Ejercicio2

---
+++ src/main/java/Ejercicio2/Web/Ejercicio2ChristianApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio2.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio2ChristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio2ChristianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ejercicio2/Web/Ejercicio2ChristianApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejercicio2.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio2ChristianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
